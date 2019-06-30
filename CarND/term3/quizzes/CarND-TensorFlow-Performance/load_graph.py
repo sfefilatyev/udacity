7adcd384d7ee79441cd45379641a221b20209e25
sfefilatyev <sfefilatyev@gmail.com>
2019-06-29 22:52:15 -0700

    [CarND Term3] Add code to tensorflow performance lesson.

---
+++ CarND/term3/quizzes/CarND-TensorFlow-Performance/load_graph.py
@@ -0,0 +1,22 @@
+def load_graph(graph_file, use_xla=False):
+    jit_level = 0
+    config = tf.ConfigProto()
+    if use_xla:
+        jit_level = tf.OptimizerOptions.ON_1
+        config.graph_options.optimizer_options.global_jit_level = jit_level
+
+    with tf.Session(graph=tf.Graph(), config=config) as sess:
+        gd = tf.GraphDef()
+        with tf.gfile.Open(graph_file, 'rb') as f:
+            data = f.read()
+            gd.ParseFromString(data)
+        tf.import_graph_def(gd, name='')
+        ops = sess.graph.get_operations()
+        n_ops = len(ops)
+        return sess.graph, ops
+
+
+sess, base_ops = load_graph('base_graph.pb')
+print(len(base_ops)) # 2165
+sess, frozen_ops = load_graph('frozen_graph.pb')
+print(len(frozen_ops)) # 245
